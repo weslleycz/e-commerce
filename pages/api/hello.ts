b1eb32fc4f72e18c595530aa24481463664a03d6
Weslley <weslleysouza22@gmail.com>
2023-01-06 16:08:32 -0300

    🏦 Estrutura do banco.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { createHandler, Body, Get, HttpCode, NotFoundException, Post, Query } from 'next-api-decorators';
 
 type Data = {
     name: string;
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            jnnnn
         </>
     );
 }
